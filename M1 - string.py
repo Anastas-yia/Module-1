75bf1fe2171a383829c1e29068769dc9646c8a3a
Anastas-yia <vredina2005a@gmail.com>
2024-10-02 23:44:04 +0300

    Add files via upload

---
+++ M1 - string.py
@@ -0,0 +1,9 @@
+example = 'Лаборатория'
+print(example[0])
+print(example[-1])
+lenght = len(example)
+start_index = (lenght+1)//2
+print(example[start_index:])
+print(example[6:])
+print(example[::-1])
+print(example[1::2])
